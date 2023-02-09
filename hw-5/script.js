322f8f63b5de071c8ec3b598713288b449e10a29
Oleksandr Oboznyi <sasha.obozny@gmail.com>
2023-02-09 23:56:18 +0200

    added hw-5

---
+++ hw-5/script.js
@@ -0,0 +1,5 @@
+const numberOne = prompt("Введіть, будь ласка, перше число");
+const numberTwo = prompt("Введіть, будь ласка, друге число");
+const numberThree = prompt("Введіть, будь ласка, третє число");
+const average = (+numberOne + +numberTwo + +numberThree) / 3;
+alert(`Середнє арифметичне введених чисел складає: ${average}`);
