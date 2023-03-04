10787de613362de3446db52d87c9027b6695fcff
Oleksandr Oboznyi <sasha.obozny@gmail.com>
2023-03-04 03:02:56 +0200

    added hw-14

--- hw-14/script.js
+++ hw-14/script.js
@@ -2,35 +2,27 @@
 
 const shoppingCart = {
     items: [],
+    
     addItem(item){
         if (this.items.length === 0){
           this.items.push(item);
-          console.log('массив пустой добавляю первый элемент');
           return this;
         } 
 
         for(let i = 0; i < this.items.length; i++){
-          console.log(this.items[i].name);
-          console.log(item.name);
           if(this.items[i].name === item.name){
             this.items[i].count += item.count;
-            console.log('Имя совпадает. Увеличиваю количество');
             return this;                  
               } 
         }
         
-        console.log('Имя не совпадает. Пушу новый элемент');
         this.items.push(item);
         return this;
     },
 
     deleteItem(item) {
       for (let i = 0; i < this.items.length; i++){
-        console.log(item);
-        console.log(this.items[i].name);
-        console.log(this.items[i].count);
         if(this.items[i].name === item && this.items[i].count === 1){
-          console.log('Delete item');
           this.items.splice(i, 1);
           return this;
         } else if (this.items[i].name === item ){
